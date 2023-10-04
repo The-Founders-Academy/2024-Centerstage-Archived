f8d262d85c54fe62c27706249ba7c2b558420813
evanjducas <evan.j.ducas@gmail.com>
2023-10-04 12:41:23 -0400

    Arm v1.0

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/FieldRelativeXDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/FieldRelativeXDrive.java
@@ -4,7 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.subsystems.XDrive;
-
+import org.firstinspires.ftc.teamcode.subsystems.Arm;
 @TeleOp(name="2024 FieldRelativeX", group="Iterative OpMode")
 public class FieldRelativeXDrive extends OpMode {
     private XDrive m_drive;
@@ -19,6 +19,7 @@ public class FieldRelativeXDrive extends OpMode {
         double velocity = Math.sqrt(Math.pow(-gamepad1.left_stick_x, 2) + Math.pow(gamepad1.left_stick_y, 2));
         double velocityAngle = Math.atan2(-gamepad1.left_stick_y, gamepad1.left_stick_x);
         double angularVelocity = gamepad1.right_stick_x;
+        Arm.MoveShoulder();
 
         m_drive.fieldRelativeDrive(velocity, velocityAngle, angularVelocity);
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Arm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Arm.java
@@ -1,5 +1,21 @@
 package org.firstinspires.ftc.teamcode.subsystems;
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.gamepad2;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 public class Arm {
-    
+    static DcMotor shoulderMotor;
+
+    public static void MoveShoulder()
+    {
+        if (gamepad2.x)
+        {
+            //Change if needed, just example number
+            shoulderMotor.setPower(-0.75);
+        }
+        else
+        {
+            shoulderMotor.setPower(0);
+        }
+    }
 }
