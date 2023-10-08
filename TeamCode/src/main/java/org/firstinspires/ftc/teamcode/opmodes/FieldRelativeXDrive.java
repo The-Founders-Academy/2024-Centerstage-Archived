9f9b9eeff4460cce7686bc3766d8cae4dd943a36
KjennethObrice <kenneth.olibrice@gmail.com>
2023-10-08 15:43:16 -0400

    I wrote some of the code we will use to test a shoulder mechanism, but I would like someone else to finish it. It's a good practice for getting used to the control loop we use for the robot and seeing how we separate subystems and opmodes.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/FieldRelativeXDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/FieldRelativeXDrive.java
@@ -8,10 +8,12 @@ import org.firstinspires.ftc.teamcode.subsystems.Arm;
 @TeleOp(name="2024 FieldRelativeX", group="Iterative OpMode")
 public class FieldRelativeXDrive extends OpMode {
     private XDrive m_drive;
+    private Arm m_arm;
 
     @Override
     public void init() {
         m_drive = new XDrive("frontLeft", "frontRight", "backleft", "backRight", null, hardwareMap);
+        m_arm = new Arm(hardwareMap);
     }
 
     @Override
@@ -19,8 +21,9 @@ public class FieldRelativeXDrive extends OpMode {
         double velocity = Math.sqrt(Math.pow(-gamepad1.left_stick_x, 2) + Math.pow(gamepad1.left_stick_y, 2));
         double velocityAngle = Math.atan2(-gamepad1.left_stick_y, gamepad1.left_stick_x);
         double angularVelocity = gamepad1.right_stick_x;
-        Arm.MoveShoulder();
 
+
+        // m_arm.rotateArm(Arm.Direction.IN, 1);
         m_drive.fieldRelativeDrive(velocity, velocityAngle, angularVelocity);
 
         // Telemetry updates down here
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Arm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Arm.java
@@ -2,20 +2,30 @@ package org.firstinspires.ftc.teamcode.subsystems;
 import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.gamepad2;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class Arm {
-    static DcMotor shoulderMotor;
+    private DcMotor shoulder; // Use the private keyword to ensure we don't accidentally use armInstance.shoulder!
 
-    public static void MoveShoulder()
-    {
-        if (gamepad2.x)
-        {
-            //Change if needed, just example number
-            shoulderMotor.setPower(-0.75);
-        }
-        else
-        {
-            shoulderMotor.setPower(0);
-        }
+    public enum Direction {
+        IN, OUT
+    }
+
+    public Arm(HardwareMap hardwareMap) {
+        shoulder = hardwareMap.get(DcMotor.class, "armShoulder");
+
+        // There is some extra setup needed for shoulder. We use DcMotor in other classes, so try to look at them to see what's missing
+        // HINT: setShoulderSpeed(speed) from the pseudocode below should rotate the arm away from the robot. We probably need to test this physically before we finish the code, but give it a try by guessing first
+
+    }
+
+    public void rotateShoulder(Direction direction, double speed) {
+        /*
+        Pseudocode
+        if direction is out:
+            setShoulderSpeed(speed)
+        else:
+            setShoulderSpeed(-speed)
+         */
     }
 }
