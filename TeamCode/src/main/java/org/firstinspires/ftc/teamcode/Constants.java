cf287fc2f4ba8e97a7cbeca341dac52c978e6cb7
KjennethObrice <kenneth.olibrice@gmail.com>
2023-10-17 06:22:49 -0400

    Added some telemetry updates so we can more easily test this branch and fixed the logic of the timeElapsed supplier.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
@@ -4,7 +4,7 @@ import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 
 public class Constants {
     public static class XDriveConstants {
-        public static final double PowerToVelocityInchesPerSecond = 1; // If we apply a power of 1 to all motors, how fast do we move?
+        public static final double PowerToVelocityInchesPerSecond = 1; // If we apply a power of 1 to all motors, how fast do we move? Find this by multiplying the speed at full speed by the percent power
     }
 
     public static class AprilTagConstants {
